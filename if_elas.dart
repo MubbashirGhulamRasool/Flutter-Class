bf55e6b2e93114c7602f8ac5263192977662a981
MubbashirGhulamRasool <appstechpk@gmail.com>
2023-06-11 21:14:10 +0500

    git class

---
+++ if_elas.dart
@@ -0,0 +1,34 @@
+void main() {
+  String email = "mubbashir@gmail.com";
+  String password = "123456";
+
+  if (email == "mubbashir@gmail.com" && password == "123456") {
+    print("Login Success ");
+  } else {
+    if (email == "mubbashir@gmail.com") {
+      print(" Email Is Correct ");
+    } else {
+      print(" Email Is NOt Correct ");
+    }
+
+    if (password == "123456") {
+      print(" Password Is Correct ");
+    } else {
+      print(" Password Is Not Correct ");
+    }
+  }
+
+// int num1 = 8;
+// int num2 = 8;
+
+// if (num1>num2) {
+//   print("Num 1 is greater than Num 2");
+// }
+// else if(num1 == num2)
+// {
+//  print("Num 1 equals to Num 2");
+// }
+// else{
+//      print("Num 2 is greater than Num 1");
+// }
+}
